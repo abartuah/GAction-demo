56af66a025252770b194a02d5d0450232ee3e2db
richard <glade450@gmail.com>
2021-10-27 20:58:43 -0400

    demo

---
+++ Main2.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region     = "us-east-2"
+  
+}
+
+resource "aws_instance" "my-server" {
+  ami           = "ami-00399ec92321828f5"
+  instance_type = "t2.micro"
+  availability_zone = "us-east-2a"
+  key_name = "hall2"
+
+  user_data = <<-EOF
+           #!/bin/bash
+           yum update -y
+           yum install httpd -y
+           systemctl start httpd
+           systenctl enable httpd
+           echo " this is my first web -server" > /var/www/html/index.html
+           EOF
+    tags = {
+      name = "web1-server"
+    }  
+}    
+
+terraform {
+       backend "remote" {
+         # The name of your Terraform Cloud organization.
+         organization = "mizaorg"
+
+         # The name of the Terraform Cloud workspace to store Terraform state files in.
+         workspaces {
+           name = "GAction-demo"
+         }
+       }
+     }
