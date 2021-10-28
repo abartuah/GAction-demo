916f5e48dca89afeac7bcdc51ff02abe6b1bc981
richard <glade450@gmail.com>
2021-10-28 00:22:13 -0400

    mydemo

--- Main2.tf
+++ Main2.tf
@@ -1,13 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 0.14"
+
+
 provider "aws" {
-  region     = "us-east-2"
-  
+  region = "us-east-2"
+
 }
 
 resource "aws_instance" "my-server" {
-  ami           = "ami-00399ec92321828f5"
-  instance_type = "t2.micro"
+  ami               = "ami-00399ec92321828f5"
+  instance_type     = "t2.micro"
   availability_zone = "us-east-2a"
-  key_name = "hall2"
+  key_name          = "hall2"
 
   user_data = <<-EOF
            #!/bin/bash
@@ -17,19 +31,19 @@ resource "aws_instance" "my-server" {
            systenctl enable httpd
            echo " this is my first web -server" > /var/www/html/index.html
            EOF
-    tags = {
-      name = "myserver"
-    }  
-}    
+  tags = {
+    name = "myserver"
+  }
+}
 
 terraform {
-       backend "remote" {
-         # The name of your Terraform Cloud organization.
-         organization = "mizaorg"
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "mizaorg"
 
-         # The name of the Terraform Cloud workspace to store Terraform state files in.
-         workspaces {
-           name = "GAction-demo"
-         }
-       }
-     }
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "GAction-demo"
+    }
+  }
+}
