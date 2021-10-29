fd34f3602490af81fcc0a33be6ee65ddeb4a4ca7
richard <glade450@gmail.com>
2021-10-29 17:41:35 -0400

    Update Main2.tf

--- Main2.tf
+++ Main2.tf
@@ -17,7 +17,7 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "my-server" {
+resource "aws_instance" "richard" {
   ami               = "ami-00399ec92321828f5"
   instance_type     = "t2.micro"
   availability_zone = "us-east-2a"
